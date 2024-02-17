962c4b7fed3d35784a5613a5cf114ec3041d1aea
gabrielvaloriani <gabriel.valoriani@gmail.com>
2024-02-17 17:32:40 +0100

    changed javascrit code and styled the buttons.

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,32 @@
-const currentOperand = document.querySelector('#current-operand');
+const currentOperand = document.querySelector('.current-operand');
 let curValue = 0;
 
-const btnDecrement = document.querySelector('#decrement');
-const btnIncrement = document.querySelector('#increment');
+const operatorsWrapper = document.getElementById('operatorsWrapper');
+
+// Creazione dei pulsanti +/- tramite JavaScript
+const btnDecrement = document.createElement('button');
+btnDecrement.textContent = '-';
+btnDecrement.classList.add('minus');
+operatorsWrapper.appendChild(btnDecrement);
+
+const btnIncrement = document.createElement('button');
+btnIncrement.textContent = '+';
+btnIncrement.classList.add('plus');
+operatorsWrapper.appendChild(btnIncrement);
+
+// Aggiunta dell'Event Delegation per gestire i click sui pulsanti tramite il wrapper
+operatorsWrapper.addEventListener('click', (event) => {
+  if (event.target.classList.contains('plus')) {
+    curValue++;
+  } else if (event.target.classList.contains('minus')) {
+    curValue--;
+  } else {
+    return; // Se il click non è su un pulsante +/-, usciamo dalla funzione
+  }
+  currentOperand.textContent = curValue;
+});
+
 const btnReset = document.querySelector('#reset');
-
-btnIncrement.addEventListener('click', () => {
-  curValue++;
-  currentOperand.textContent = curValue;
-});
-
-btnDecrement.addEventListener('click', () => {
-  curValue--;
-  currentOperand.textContent = curValue;
-});
-
 btnReset.addEventListener('click', () => {
   curValue = 0;
   currentOperand.textContent = curValue;
