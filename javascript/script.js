76e0d3881f365a7d1506a483f82497a62b836d7c
gabrielvaloriani <gabriel.valoriani@gmail.com>
2024-02-18 12:16:33 +0100

    Changed javascript file

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,7 @@ let curValue = 0;
 
 const operatorsWrapper = document.getElementById('operatorsWrapper');
 
-// Creazione dei pulsanti +/- tramite JavaScript
+
 const btnDecrement = document.createElement('button');
 btnDecrement.textContent = '-';
 btnDecrement.classList.add('minus');
@@ -14,14 +14,13 @@ btnIncrement.textContent = '+';
 btnIncrement.classList.add('plus');
 operatorsWrapper.appendChild(btnIncrement);
 
-// Aggiunta dell'Event Delegation per gestire i click sui pulsanti tramite il wrapper
 operatorsWrapper.addEventListener('click', (event) => {
   if (event.target.classList.contains('plus')) {
     curValue++;
   } else if (event.target.classList.contains('minus')) {
     curValue--;
   } else {
-    return; // Se il click non è su un pulsante +/-, usciamo dalla funzione
+    return; 
   }
   currentOperand.textContent = curValue;
 });
